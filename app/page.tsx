9825abc50ef531ccfd3f19b2c49e22cfd6b6c2da
arya <aryavl9813@gmail.com>
2024-02-12 00:03:25 +0530

    Added navbar

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+"use client"
+import MobileNav from "@/components/MobileNav";
+import Nav from "@/components/Nav";
+import Image from "next/image";
+import { useState } from "react";
+
+export default function Home() {
+  const [nav,setNav] = useState(false)
+  const openNav = () => setNav(true)
+  const closeNav = () => setNav(false)
+  return (
+   <div className="overflow-x-hidden">
+    <div>
+      <MobileNav nav={nav} closeNav={closeNav}/>
+      <Nav openNav={openNav}/>
+    </div>
+   </div>
+  );
+}
